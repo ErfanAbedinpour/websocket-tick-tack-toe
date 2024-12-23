fa32f3ea77cdb1ca6a447e999421695919424943
erfan <erfan.wtf44@gmail.com>
2024-12-23 11:54:48 -0500

    adding tic tack toe cell To frontEnd Page

--- src/server/services/redis.ts
+++ src/server/services/redis.ts
@@ -4,6 +4,7 @@ import { Redis } from 'ioredis';
 
 export class RedisService {
     private redis: Redis
+
     constructor() {
         this.redis = new Redis({ host: 'redis' });
         // disconnect when redis dissconncted
